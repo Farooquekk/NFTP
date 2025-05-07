7d22d4a618c5b6b0917688f5b0a4a7638d2c935d
Farooquekk <22sw040@students.muet.edu.pk>
2025-05-07 08:24:00 -0700

    01_Day
    First Class of PHP (NFTP)

---
+++ 02_PHP/01_DAY/welcome.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 10;
+echo("Welcome to PHP DAY_01");
+print("<br>");
+$name = "Farooque Sajjad";
+print("Hello");
+print("<br>");
+print($a);
+
+?>
+<?php echo "<h1>$name</h1>";
+
+$i=0;
+while($i<10){
+    echo "<h1>$name</h1>";
+    $i++;
+}
+?>
