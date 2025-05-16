1f6834efe4b774093cf24b3b1af89a301a2b95fa
Farooquekk <22sw040@students.muet.edu.pk>
2025-05-16 09:36:47 -0700

    07_DAY
    Seventh Class of PHP in NFTP

---
+++ 02_PHP/07_DAY/index.php
@@ -0,0 +1,22 @@
+
+
+<?php
+if(true){
+    echo "This is Heplicopter";
+}
+
+while(true){
+    echo "This is while looop";
+    echo "<br>"
+}
+
+for($i=0;$i<10;$i++){
+
+}
+
+$students[]=array("A","B","C","D");
+$stu = array("1","2","3","4");
+
+
+// first loop created was while loop
+?>
