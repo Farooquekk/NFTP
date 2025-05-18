489132a6c658518eb4df76670615bdd5d62a75b1
Farooquekk <22sw040@students.muet.edu.pk>
2025-05-18 04:13:41 -0700

    09_Class
    Ninth Class of PHP in NFTP

---
+++ 02_PHP/09_DAY/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Enter Number of Subjects</h1>
+
+<form action="page2.php" method="post">
+    <label for="Enter Number">Enter Number</label>
+    <input type="number" name="number">
+    <br>
+    <input type="submit" value="Next">
+</form>
+    
+</body>
+</html>
---
+++ 02_PHP/09_DAY/page2.php
@@ -0,0 +1,14 @@
+<?php
+$num = $_POST['number'];
+
+
+?>
+
+<form action="page3.php" method="post">
+
+<table border="1">
+<th>Gender</th>
+
+</table>
+
+</form>
